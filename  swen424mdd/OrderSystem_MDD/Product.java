a84f70c3a2ac1d4dae6b8beb25a4e1f86ad98762
alvindaiyan@gmail.com <alvindaiyan@gmail.com@658e228e-6d80-8e3b-9bbb-806fed7d61dc>
2013-09-17 05:04:07 +0000

    initial commit

---
+++  swen424mdd/OrderSystem_MDD/Product.java
@@ -0,0 +1,65 @@
+// From MetaEdit+, graph: ProductOrdering
+// .java file for class Product
+
+public class Product {
+// member data
+	// private
+	private Supplier supplier;	//  This is the supplier of the product
+	private String name;	//  This is the name of the product.
+	private double price=0;	//  This is the price of the product
+	// MEPMD5 Protected block for private attributes	
+	// Enter own private attributes here
+	// MEPMD5 369f4dc4709c509ebcc08fc6e7fb8d9f	
+	// protected
+	// MEPMD5 Protected block for protected attributes	
+	// Enter own protected attributes here
+	// MEPMD5 89050b9f70f02e5bb34d5d0187e36d5c	
+	// public
+	// MEPMD5 Protected block for public attributes	
+	// Enter own public attributes here
+	// MEPMD5 b34fd6890c13447591ccca766bacea04	
+
+// member functions
+	public Supplier getSupplier() {
+		// MEPMD5 29_572 protected block for method body	
+		return supplier;
+		// MEPMD5 c18024449b3a652a2e4db6b286081063	
+	}
+
+	public void setSupplier(Supplier supplier) {
+		// MEPMD5 29_548 protected block for method body	
+		supplier=supplier;
+		// MEPMD5 83afa64e52b92e18e911e374123774d1	
+	}
+
+	public void setPrice(double price) {
+		// MEPMD5 29_514 protected block for method body	
+		price=price;
+		// MEPMD5 cb7dbfca451bb4cbca9ae38810d52cf3	
+	}
+
+	public double getPrice() {
+		// MEPMD5 29_480 protected block for method body	
+		return price;
+		// MEPMD5 a238f72b3659896e0ea70274fe8eaec6	
+	}
+
+	public String getName() {
+		// MEPMD5 29_422 protected block for method body	
+		return name;
+		// MEPMD5 ad665ac794e96daff4cf2920fcdca01f	
+	}
+
+	public void setName(String name) {
+		// MEPMD5 29_456 protected block for method body	
+		name=name;
+		// MEPMD5 abf479811f73f46fcb4401777078d9e0	
+	}
+
+	// Protected block for manually added member functions
+	// MEPMD5 29_596 manually added member functions	
+	// Enter code here
+	// MEPMD5 d562e6df93aed13645b67acd34cf7773	
+
+} // end of class Product
+
---
+++  swen424mdd/OrderSystem_MDD/Supplier.java
@@ -0,0 +1,55 @@
+import java.util.List;
+
+// From MetaEdit+, graph: ProductOrdering
+// .java file for class Supplier
+
+public class Supplier {
+// member data
+	// private
+	private List products;	//  This is the product of the supplier.
+	private String name;	//  This is the name of the supplier
+	// MEPMD5 Protected block for private attributes
+	// Enter own private attributes here
+	// MEPMD5 369f4dc4709c509ebcc08fc6e7fb8d9f
+	// protected
+	// MEPMD5 Protected block for protected attributes
+	// Enter own protected attributes here
+	// MEPMD5 89050b9f70f02e5bb34d5d0187e36d5c
+	// public
+	// MEPMD5 Protected block for public attributes
+	// Enter own public attributes here
+	// MEPMD5 b34fd6890c13447591ccca766bacea04
+	Address address;	// aggregation based data
+
+// member functions
+	public List getProducts() {
+		// MEPMD5 29_1645 protected block for method body
+		return products;
+		// MEPMD5 1f332ac0ee79ef2e2d3b79048ee7e71c
+	}
+
+	public void setProducts(List products) {
+		// MEPMD5 29_1621 protected block for method body
+		products=products;
+		// MEPMD5 37e16f076af7422c90980e499e1c93b5
+	}
+
+	public String getName() {
+		// MEPMD5 29_1529 protected block for method body
+		return name;
+		// MEPMD5 ad665ac794e96daff4cf2920fcdca01f
+	}
+
+	public void setName(String name) {
+		// MEPMD5 29_1505 protected block for method body
+		name=name;
+		// MEPMD5 abf479811f73f46fcb4401777078d9e0
+	}
+
+	// Protected block for manually added member functions
+	// MEPMD5 29_1669 manually added member functions
+	// Enter code here
+	// MEPMD5 d562e6df93aed13645b67acd34cf7773
+
+} // end of class Supplier
+
